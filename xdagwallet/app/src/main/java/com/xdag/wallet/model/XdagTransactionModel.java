ab741c65c6f70b97b0348908efc5612400092a8a
wangxuguo <wangxuguo@yeah.net>
2018-07-04 08:25:40 +0800

    wallet UI

---
+++ xdagwallet/app/src/main/java/com/xdag/wallet/model/XdagTransactionModel.java
@@ -0,0 +1,12 @@
+package com.xdag.wallet.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by wangxuguo on 2018/7/4.
+ */
+
+public class XdagTransactionModel implements Serializable{
+
+    private double amount;
+}
